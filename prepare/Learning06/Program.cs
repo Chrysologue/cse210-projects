7963afbd01fe01a050237d39a95607bc46de84ad
Chrysologue <chrysobeatz@gmailcom>
2024-12-09 20:31:17 +0300

    updating code

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("Blue", 5.23);
+        Rectangle rectangle = new Rectangle("Red", 6.12, 4.88);
+        Circle circle = new Circle("Green", 3.89);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} shape has area of {shape.GetArea()}");
+        }
     }
 }
